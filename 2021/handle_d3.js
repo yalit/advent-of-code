33f2af0978541a54dfdc589db92874a58ac86ef3
Yannick Alsberge <y.alsberge@yalit.be>
2021-12-05 16:40:20 +0100

    day 3 done

---
+++ 2021/handle_d3.js
@@ -0,0 +1,79 @@
+import getInput from "./getInput.js"
+
+function handleInput_1(lines){
+    let gamma = ''
+    let epsilon = ''
+
+    let mapped = []
+    lines.forEach((line, k) => {
+        if (k ===0 ){
+            mapped = line.split('').map(d => [d])
+            return
+        }
+
+        line.split('').forEach((element, k) => {
+            mapped[k].push(element)
+        });
+    });
+
+    gamma = parseInt(mapped.map(elems => findMostCommonBit(elems)).join(''), 2)
+    epsilon = parseInt(mapped.map(elems => findLeastCommonBit(elems)).join(''), 2)
+
+    return gamma * epsilon
+}
+
+
+function handleInput_2(lines){
+    let oxygen = lines, co2 = lines
+    let n = 0
+
+    while (oxygen.length > 1 && n < lines[0].length){
+        let bits = getNthBitsFromElements(oxygen, n)
+        oxygen = rating(oxygen, n, findMostCommonBit(bits))
+        n++
+    }
+    oxygen = parseInt(oxygen[0], 2)
+
+    n = 0
+    while (co2.length > 1 && n < lines[0].length){
+        let bits = getNthBitsFromElements(co2, n)
+        co2 = rating(co2, n, findLeastCommonBit(bits))
+        n++
+    }
+    co2 = parseInt(co2[0], 2)
+
+    return oxygen * co2
+}
+
+function rating(elements, bitPosition, value){
+    return elements.filter(elem => elem.split('')[bitPosition] === value)
+}
+
+
+function getNthBitsFromElements(elements, n){
+    return elements.map(elem => elem.split('')[n])
+}
+
+function findMostCommonBit(elements){
+    let common = 0
+    elements.forEach(element => {
+        if (element === '0') common -=1
+        else if (element === '1') common +=1
+    });
+
+    return (common >= 0) ? '1' : '0'
+}
+
+function findLeastCommonBit(elements){
+    let common = 0
+    elements.forEach(element => {
+        if (element === '0') common +=1
+        else if (element === '1') common -=1
+    });
+
+    return (common > 0) ? '1' : '0'
+}
+
+export function handleInput(lines) {
+    return [handleInput_1(lines), handleInput_2(lines)]
+}
