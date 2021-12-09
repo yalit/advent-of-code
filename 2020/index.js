0e69e69c4a8f9f9506534d519bcd1e3761d3bd27
Yannick Alsberge <y.alsberge@yalit.be>
2021-12-09 20:34:56 +0100

    setup generic js and input files

---
+++ 2020/index.js
@@ -0,0 +1,23 @@
+import {createInterface} from 'readline'
+import {stdin, stdout} from 'process'
+import getInput from '../getInput.js'
+
+const readConsole = createInterface({
+    input: stdin,
+    output: stdout
+})
+
+readConsole.question('Which day do you want to test ?', (day) => {
+    let input = ''
+    getInput(day)
+        .then(data => {
+            input = data
+            return import("./handle_d"+String(day)+".js")
+        }).then(({handleInput}) => {
+            console.log(handleInput(input.trim().split('\n')))
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    readConsole.close()
+})
