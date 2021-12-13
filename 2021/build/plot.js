953218e92c1eff7f2a9c0106d069307227995c93
Yannick Alsberge <y.alsberge@yalit.be>
2021-12-13 21:50:36 +0100

    remove build folder

--- 2021/build/plot.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.plotLine = void 0;
-const babar_1 = require("babar");
-function plotLine(data) {
-    console.log((0, babar_1.babar)(data));
-}
-exports.plotLine = plotLine;
