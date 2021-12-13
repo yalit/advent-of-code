353213f91046ea1c6ab47637d996a3cf30cccf1f
Yannick Alsberge <y.alsberge@yalit.be>
2021-12-13 20:59:16 +0100

    Update structure of previous year - refactoring ts

--- 2020/src/index.ts
+++ 2020/src/index.ts
@@ -1,16 +1,16 @@
 import {createInterface} from 'readline'
 import {stdin, stdout} from 'process'
-import getInput from '../getInput.js'
+import getInput from './getInput.js'
 
 const readConsole = createInterface({
     input: stdin,
     output: stdout
 })
 
-readConsole.question('Which day do you want to test ?', (day) => {
-    let input = ''
+readConsole.question('Which day do you want to test ?', (day: string) => {
+    let input:string  = ''
     getInput(day)
-        .then(data => {
+        .then((data: string) => {
             input = data
             return import("./handle_d"+String(day)+".js")
         }).then(({handleInput}) => {
