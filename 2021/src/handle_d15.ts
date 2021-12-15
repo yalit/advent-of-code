97cf5c074ff15b427cb80fcbf40a0189b221cb56
Yannick Alsberge <y.alsberge@yalit.be>
2021-12-15 14:23:21 +0100

    2021 day 15 part 1 dijkstra with priority queue

--- 2021/src/handle_d15.ts
+++ 2021/src/handle_d15.ts
@@ -3,16 +3,51 @@ interface Coord {
     y: number
 }
 
-interface Vertice {
-    weight: number,
+function index(coord: Coord): string {
+    return JSON.stringify(coord)
+}
+
+interface Cost {
+    cost: number,
     to: Coord
 }
 
-class Node {
-    vertices: Array<Vertice> = []
+interface Graph {
+    neighbors: {[key: string]: Array<Cost>}
+}
 
-    addVertice = function(vertice: Vertice) {
-        this.vertices.push(vertice)
+interface QElement {
+    coord: Coord,
+    priority: number
+}
+
+class PriorityQueue {
+    items: Array<QElement> = []
+
+    put = function(coord: Coord, priority: number) {
+        const qElement = {coord, priority}
+        
+        let added = false
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].priority > priority) {
+                this.items.splice(i, 0, qElement)
+                added = true
+                break
+            }
+        }
+
+        if (!added) {
+            this.items.push(qElement)
+        }
+        
+    }
+
+    get = function(): Coord {
+        return this.items.shift().coord
+    }
+
+    empty = function(): boolean {
+        return this.items.length === 0
     }
 }
 
@@ -26,71 +61,69 @@ function getAllNeighbors(coord: Coord, width: number, height: number): Array<Coo
     return neighbors
 }
 
-function parse(lines: Array<string>): [Map<string, Node>, number, number] {
-    let tree = new Map()
+function heuristic(a: Coord, b: Coord): number {
+    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
+}
+
+function parse(lines: Array<string>): [Graph, number, number] {
+    let tree: Graph = {neighbors: {}}
     const width = lines[0] ? lines[0].length : 0
     const height = lines.length
 
     for (let y=0; y < lines.length; y++) {
         const tLine = lines[y].split('').map(c => Number(c))
         for (let x = 0; x < tLine.length; x++) {
-            let node = new Node()
             const currentCoord: Coord =  {x, y}
+
+            tree.neighbors[index(currentCoord)] = []
             getAllNeighbors(currentCoord, width, height).forEach((coord: Coord) => {
-                node.addVertice({to: coord, weight: Number(lines[coord.y][coord.x])})
+                tree.neighbors[index(currentCoord)].push({to: coord, cost: Number(lines[coord.y][coord.x])})
             })
-            tree.set(index(currentCoord), node)
         }
     }
 
     return [tree, width, height]
 }
 
-function index(coord: Coord): string {
-    return JSON.stringify(coord)
-}
-
 function handleInput_1(lines: Array<string>){
-    let [tree, width, height]: [Map<string, Node>, number, number] = parse(lines)    
+    let [tree, width, height]: [Graph, number, number] = parse(lines)    
     const max = width*height*10*10
-    const start:string =  index({x: 0, y:0})
-    const end: string = index({x:width-1, y:height-1})
+    const startCoord: Coord = {x: 0, y:0}
+    const endCoord: Coord = {x:width-1, y:height-1}
+    const start:string =  index(startCoord)
+    const end: string = index(endCoord)
+    
+    let toVisit: PriorityQueue = new PriorityQueue()
+    toVisit.put({x: 0, y:0}, 0)
 
-    let visited: Array<string> = []
-    let distances: Map<string, number> = new Map()
+    let came_from: {[key: string]: Coord} = {}
+    came_from[start] = undefined
 
-    tree.forEach((node: Node, i: string) => distances.set(i, undefined))
-    distances.set(start, 0)
+    let cost_so_far: {[key: string]: number} = {}
+    cost_so_far[start] = 0
 
-    let currentIndex = start
-    let endFound = false
-    while(visited.length < tree.size && ! endFound) {
-        console.log(tree.size, visited.length);
+    let currentCoord: Coord
+    while (!toVisit.empty()) {
+        currentCoord = toVisit.get()
         
-        //compute min size for all neighbors
-        tree.get(currentIndex).vertices.forEach((v: Vertice) => {
-            const weight: number = distances.get(currentIndex) + v.weight
-            if (distances.get(index(v.to)) === undefined) distances.set(index(v.to), weight)
-            else if (distances.get(index(v.to)) > weight) distances.set(index(v.to), distances.get(currentIndex) + v.weight)
-        })
 
-        //add currentIndex to the visited nodes
-        visited.push(currentIndex)
-        tree.delete(currentIndex)
+        if (index(currentCoord) === end) {
+            break
+        }
 
-        endFound = currentIndex === end
-
-        //get Next index with smaller distance
-        let smallestDistance = max
-        Array.from(tree.keys()).forEach((k : string) => {
-            if (distances.get(k) < smallestDistance) {
-                smallestDistance = distances.get(k)
-                currentIndex = k
+        const currentIndex = index(currentCoord)
+        tree.neighbors[currentIndex].forEach((cost: Cost)=> {
+            const new_cost = cost_so_far[currentIndex] + cost.cost
+            
+            if (!Object.keys(cost_so_far).includes(index(cost.to)) || new_cost < cost_so_far[index(cost.to)]){
+                cost_so_far[index(cost.to)] = new_cost
+                toVisit.put(cost.to, new_cost)
+                came_from[index(cost.to)] = currentCoord
             }
-        })
+        })        
     }
-
-    return distances.get(end)
+    
+    return cost_so_far[end]
 }
 
 function handleInput_2(lines: Array<string>){
