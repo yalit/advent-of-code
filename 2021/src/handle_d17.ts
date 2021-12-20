ec3677d5322736fd366ad8999f9a07f4a4e149d8
Yannick Alsberge <y.alsberge@yalit.be>
2021-12-20 16:56:50 +0100

    2021 day 17 done

--- 2021/src/handle_d17.ts
+++ 2021/src/handle_d17.ts
@@ -1,63 +1,73 @@
 import {_} from 'lodash'
 
-function sumN(n: number): number {
-    return (n*(n+1))/2
-}
-
-type findType = 'min' | 'max'
-
-function getNForD(d: number): number {
-    let x = 1
-
-    while (sumN(x) < d) {
-        x++
-    }
-
-    
-    return (sumN(x) === d) ? x : x - 1
-}
-
-function getRawNForD(d: number): number {
-    return Math.sqrt(1+ (2 * d)) - 1
-}
-
-function getMaxDistanceY(y: number, lowY: number): number {
-    return sumN(y) - lowY
-}
-
 function parseLine(line: string): Array<number> {
     const n = line.match(/([?-\d]+)/g)
     
     return n.map(c => Number(c))
 }
 
-function searchY(x: number, highY: number, lowY: number): number {
-    let y = x - 1
-    let nMinY: number = 0
+type Coord = {x: number, y: number}
+type Target = {topleft: Coord, bottomright: Coord}
 
-    while (sumN(nMinY + 1) <= getMaxDistanceY(y, lowY)) {
-        y++
-        nMinY = getNForD(getMaxDistanceY(y, highY))
-    }
+function getTarget(line: string): Target {
+    const inputs = parseLine(line)
 
-    return y - 1
+    const left = Math.min(inputs[0], inputs[1])
+    const right = Math.max(inputs[0], inputs[1])
+    const top = Math.max(inputs[2], inputs[3])
+    const bottom = Math.min(inputs[2], inputs[3])
+
+    return {topleft: {x: left, y: top}, bottomright: {x: right, y: bottom}}
 }
 
-function handleInput_1(lines: Array<string>){
-    const [aX, bX, aY, bY]: Array<number> = parseLine(lines[0])
-    const minX = Math.min(aX, bX)
-    const maxX = Math.max(aX, bX)
-    const highY = Math.max(aY, bY)
-    const lowY = Math.min(aY, bY)
+function isXinTarget(coord: Coord, target: Target ): boolean {
+    return coord.x <= target.bottomright.x && coord.x >= target.topleft.x
+}
 
-    const x = Math.min(getNForD(minX) + 1, getNForD(maxX))
-    
-    return searchY(x, highY, lowY)
-    //return _.range(15).map(n => sumN(n))
+function isYinTarget(coord: Coord, target: Target): boolean {
+    return coord.y <= target.topleft.y && coord.y >= target.bottomright.y
+}
+
+function isCoordInTarget(coord: Coord, target: Target) {
+    return isXinTarget(coord, target) && isYinTarget(coord, target)
+}
+
+function getAllCoordsForXAndY(x: number, y: number, target: Target): Array<Coord> {
+    let coords = []
+    let coord = {x: 0, y: 0}
+    let stepNumber = 0
+    while (coord.x <= target.bottomright.x && coord.y >= target.bottomright.y) {
+        let addX = x - stepNumber <= 0 ? 0 : x - stepNumber
+        let addY = y - stepNumber
+        coord = {x: coord.x + addX, y: coord.y + addY}
+        
+        if (isCoordInTarget(coord, target)) coords.push(coord)
+        
+        stepNumber++
+    }
+
+    return coords
+}
+
+function handleInput_1(lines: Array<string>){ 
+    const target = getTarget(lines[0])
+    return _.sum(_.range(Math.abs(target.bottomright.y))) //always arriving at 0, so the biggest step that can be done is Math.abs(lowest step - 1)
 }
 
 function handleInput_2(lines: Array<string>){
-    return 0
+    const target = getTarget(lines[0])
+    
+    let y = target.bottomright.y
+    let nbTrajectories = 0
+    let coords: Array<Coord>
+    for (let y = target.bottomright.y; y <= Math.abs(target.bottomright.y); y++){
+        for (let x = 0; x <= target.bottomright.x; x++) {
+            coords = getAllCoordsForXAndY(x,y,target)
+            if (coords.length > 0) nbTrajectories++
+        }
+    }
+
+    return nbTrajectories
 }
 
 
