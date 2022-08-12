744b8761ea7f11d44141f19c3b09765612018295
Yannick Alsberge <y.alsberge@yalit.be>
2022-08-12 09:35:37 +0200

    2015 : day 8 finished

--- 2015/src/handle_d8.ts
+++ 2015/src/handle_d8.ts
@@ -1,21 +1,74 @@
-function parseString(s: string): number {
-    const hexa = /(\\x)/g
-    const doublequotes = /(\\\")/g
-    const doubleback = /(\\\\)/g
-    
-    const hexaNb = s.match(hexa) ?? []
-    const dbQuotesNb = s.match(doublequotes) ?? []
-    const dbBackslashNb = s.match(doubleback) ?? []
-    
-    return dbQuotesNb.length + dbBackslashNb.length + (3 * hexaNb.length) + 2
+function parseStringLiteral(s: string): number {
+   s = s.slice(1,s.length-1) //remove beginning and end "
+
+    let i: number = 0
+    let stringCharactersCount: number = 0
+    let escapeFound: boolean = false
+    while (i < s.length) {
+        let char = s[i]
+        if (!escapeFound && char !== "\\") {
+            stringCharactersCount++
+            i++ 
+        } else if(!escapeFound && char === "\\") {
+            escapeFound = true
+            i++
+        } else if (char === "\"" || char === "\\") {
+            stringCharactersCount++
+            escapeFound = false
+            i++
+        } else if (char === "x") {
+            stringCharactersCount++
+            escapeFound = false
+            i += 3
+        } else {
+            throw `Not possible to arrive here : ${s} / ${i} / ${char}`
+        }
+    }
+
+    return s.length + 2 - stringCharactersCount
 }
 
+function parseStringLiteral_2(s: string): number {
+    s = s.slice(1,s.length-1) //remove beginning and end "
+ 
+     let i: number = 0
+     let addedEncodingChars: number = 0
+     let escapeFound: boolean = false
+     while (i < s.length) {
+         let char = s[i]
+         if (!escapeFound && char !== "\\") {
+             i++ 
+         } else if(!escapeFound && char === "\\") {
+             escapeFound = true
+             addedEncodingChars++
+             i++
+         } else if (char === "\"" || char === "\\") {
+             addedEncodingChars++
+             escapeFound = false
+             i++
+         } else if (char === "x") {
+             escapeFound = false
+             i += 3
+         } else {
+             throw `Not possible to arrive here : ${s} / ${i} / ${char}`
+         }
+     }
+ 
+     return 4 + addedEncodingChars
+ }
+
 function handleInput_1(lines: Array<string>){
-    return lines.map(parseString).reduce((s: number, x:number) => s+x,0)
+    const results = lines.map(parseStringLiteral)
+    console.log(results);
+    
+    return results.reduce((s: number, x:number) => s+x,0)
 }
 
 function handleInput_2(lines: Array<string>){
-    return 0
+    const results = lines.map(parseStringLiteral_2)
+    console.log(results);
+    
+    return results.reduce((s: number, x:number) => s+x,0)
 }
 
 
