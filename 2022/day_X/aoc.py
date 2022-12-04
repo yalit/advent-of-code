2f711a91b340791e437a13635a9855c4af0e7e16
Yannick Alsberge <y.alsberge@yalit.be>
2022-12-04 09:36:31 +0100

    build : update the default aoc.py

--- 2022/day_X/aoc.py
+++ 2022/day_X/aoc.py
@@ -1,6 +1,6 @@
-def handle_part_1(lines: list[str]) -> str:
-    return ""
+def handle_part_1(lines: list[str]) -> int:
+    return 0
 
 
-def handle_part_2(lines: list[str]) -> str:
-    return ""
+def handle_part_2(lines: list[str]) -> int:
+    return 0
