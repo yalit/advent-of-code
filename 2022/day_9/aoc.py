9d6d81c81302105cd9b7a03d229ac66dfc0fc747
Yannick Alsberge <y.alsberge@yalit.be>
2022-12-09 10:29:15 +0100

    aoc : 2022 day 9

--- 2022/day_9/aoc.py
+++ 2022/day_9/aoc.py
@@ -10,20 +10,7 @@ movesHead = {
     'D': lambda c: [c[0], c[1] - 1],
 }
 
-
-def seePath(arr: list[list]):
-    width = reduce(lambda w, e: abs(e[0]) if abs(e[0]) > w else w, arr, 0) + 2
-    height = reduce(lambda w, e: abs(e[1]) if abs(e[1]) > w else w, arr, 0) + 2
-    t = [['.' for _ in range(width * 2 + 1)] for _ in range(height * 2 + 1)]
-    t[height][width] = 's'
-    for idx, k in enumerate(arr[:-1]):
-        t[height + k[1]][width + k[0]] = len(arr) - idx - 1
-
-    t[height + arr[-1][1]][width + arr[-1][0]] = 'H'
-    visualize(list(reversed(t)))
-
-
-def shouldMove(c1: list[int], c2: list[int]) -> str:
+def whichMove(c1: list[int], c2: list[int]) -> str:
     if c2[0] == c1[0] or c1[1] == c2[1]:
         return 'line' if ((abs(c2[0] - c1[0]) if c2[0] > c1[0] else abs(c1[0] - c2[0])) > 1 or (abs(c2[1] - c1[1]) if c2[1] > c1[1] else abs(c1[1] - c2[1])) > 1) else 'no'
 
@@ -32,10 +19,10 @@ def shouldMove(c1: list[int], c2: list[int]) -> str:
 
 def moveKnot(prev: list, knot: list):
     mtx, mty = 0, 0
-    if shouldMove(knot, prev) == 'line':
+    if whichMove(knot, prev) == 'line':
         mtx = 1 if prev[0] > knot[0] + 1 else -1 if prev[0] < knot[0] - 1 else 0
         mty = 1 if prev[1] > knot[1] + 1 else -1 if prev[1] < knot[1] - 1 else 0
-    elif shouldMove(knot, prev) == 'diag':
+    elif whichMove(knot, prev) == 'diag':
         mtx = 1 if prev[0] > knot[0] else -1 if prev[0] < knot[0] else 0
         mty = 1 if prev[1] > knot[1] else -1 if prev[1] < knot[1] else 0
     return [knot[0] + mtx, knot[1] + mty]
@@ -56,12 +43,6 @@ def handleRopeTravel(rope: list[list], moves: list[str], debug: bool = False) ->
             rope = handleHeadMove(rope, l[0:1])
             if rope[0] not in visited:
                 visited.append(rope[0])
-
-            if debug:
-                print('#########################################')
-                print(rope)
-                print(f'New Step')
-                seePath(rope)
     return visited
 
 
