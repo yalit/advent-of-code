e501ebc971721c0582e28f4a5a9787622d4875f3
Yannick Alsberge <y.alsberge@yalit.be>
2022-12-11 17:40:21 +0100

    aoc : 2022 day 11

--- 2022/day_11/aoc.py
+++ 2022/day_11/aoc.py
@@ -1,4 +1,5 @@
 import re
+from functools import reduce
 
 
 class Monkey:
@@ -6,31 +7,52 @@ class Monkey:
         inputs = input.split('\n')
         self.items = [int(x) for x in inputs[1][18:].split(', ')]
         self.op = inputs[2][19:]
-        self.test = {'test': int(inputs[3][22:]), 'true': int(inputs[4][29:]), 'false': int(inputs[5][30])}
+        self.test = {'test': int(inputs[3][21:]), 'true': int(inputs[4][29:]), 'false': int(inputs[5][30])}
+        self.inspections = 0
 
+    def treatItem(self, mod: int = 1):
+        if len(self.items) == 0:
+            return
+        self.inspections += 1
+        item = self.items[0]
+        self.items = self.items[1:]
+        n = self.handleWorryLevel(self.getNewValue(item), mod)
+        recipient = self.test['true'] if n % self.test['test'] == 0 else self.test['false']
+        return [recipient, n]
 
-    def getItemsRecipients(self):
-        recipients = {}
-        for item in self.items:
-            n = self.getNewValue(item)
-            if n % self.test['test'] == 0:
-                recipients[self.test['true']] = n
-            else:
-                recipients[self.test['false']] = n
-        return recipients
-    
+    def handleWorryLevel(self, v: int, mod: int = 1):
+        return v // 3
+
+    def addItem(self, item):
+        self.items.append(item)
+
     def getNewValue(self, v: int):
         data = re.compile(r'^(?P<one>\d+).(?P<op>[+.*/]).(?P<two>\d+)$').fullmatch(self.op.replace('old', str(v)))
         o = {'+': lambda a, b: a + b, '-': lambda a, b: a - b, '*': lambda a, b: a * b, '/': lambda a, b: a / b}
         return o[data.groupdict()['op']](int(data.groupdict()['one']), int(data.groupdict()['two']))
 
-    
+
+class UnManagedMonkey(Monkey):
+    def handleWorryLevel(self, v: int, mod: int = 1):
+        return v % mod
+
+
+def handleInspections(monkeys: list[Monkey], nb: int, mod: int = 1):
+    for _ in range(nb):
+        for monkey in monkeys:
+            for item in monkey.items:
+                r = monkey.treatItem(mod)
+                monkeys[r[0]].addItem(r[1])
+
 
 def handle_part_1(lines: list[str]) -> int:
     monkeys = [Monkey(x) for x in '\n'.join(lines).split('\n\n')]
-    print(monkeys[0].getItemsRecipients())
-    return 0
+    handleInspections(monkeys, 20)
+    return reduce(lambda a, b: a * b, sorted([m.inspections for m in monkeys])[-2:])
 
 
 def handle_part_2(lines: list[str]) -> int:
-    return 0
+    monkeys = [UnManagedMonkey(x) for x in '\n'.join(lines).split('\n\n')]
+    fullModulo = reduce(lambda a, b: a*b, [m.test['test'] for m in monkeys], 1)
+    handleInspections(monkeys, 10000, fullModulo)
+    return reduce(lambda a, b: a * b, sorted([m.inspections for m in monkeys])[-2:])
