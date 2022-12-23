3349919c6a9506f3bab10bc5c393b9ff139c6554
Yannick Alsberge <y.alsberge@yalit.be>
2022-12-23 09:32:43 +0100

    aoc : 2022 day 22 full

--- 2022/day_22/aoc.py
+++ 2022/day_22/aoc.py
@@ -26,21 +26,26 @@ def move(lines, wrapCallback):
 
     # x, y coordinate relative to the full length of the  grid
     # sx coordinate relative to the length of the shape for each line
-    x, y, sx, d = grid[0].index('.'), 0, 0, 'R'
-    print('x', x, 'sx', sx, 'y', y, d)
+    x, y, d = grid[0].index('.'), 0, 'R'
+    print('x', x, 'y', y, d)
 
     for move in moves:
         if move in ['R', 'L']:
             d = changeDir[move][d]
             continue
 
-        dx, dy = dir_moves[d]
+        print(d, move)
         for s in range(1, move + 1):  # starts at 1 ends at move
-            nx, ny = wrapCallback(grid, gh, gw, x, y, dir_moves[d])
+            nx, ny, nd = wrapCallback(grid, gh, gw, x, y, d)
 
-            if grid[ny][nx] == '#':
-                break
-            x, y = nx, ny
+            try:
+                if grid[ny][nx] == '#':
+                    break
+            except:
+                print("Error", x, y, nx, ny, d)
+                raise KeyError((nx, ny, nd))
+            x, y, d = nx, ny, nd
+            print(x, y, d)
 
     print(x, y)
     return (1000 * (y + 1)) + (4 * (x + 1)) + directions.index(d)
@@ -48,7 +53,7 @@ def move(lines, wrapCallback):
 
 def handle_part_1(lines: list[str]) -> int:
     def wrapCallback(grid, gh, gw, x, y, direction):
-        dx, dy = direction
+        dx, dy = dir_moves[direction]
         nx, ny = x + dx, y + dy
 
         if nx < 0 or nx >= gw or grid[y][nx] == ' ' or ny < 0 or ny >= gh or grid[ny][x] == ' ':
@@ -56,10 +61,123 @@ def handle_part_1(lines: list[str]) -> int:
             while 0 <= fx < gw and grid[y][fx] != ' ' and 0 <= fy < gh and grid[fy][x] != ' ':
                 fx, fy = fx - dx, fy - dy
             nx, ny = fx + dx, fy + dy
-        return nx, ny
+        return nx, ny, direction
 
     return move(lines, wrapCallback)
 
 
 def handle_part_2(lines: list[str]) -> int:
-    return 0
+
+    def wrapCallbackTestInput(grid, gh, gw, x, y, direction):
+        l = 4
+
+        #       1
+        #   1 2| |6
+        # 3| | | |7
+        #   4 5| | |6
+        #       4 3
+
+        # 1 top to 1 bottom
+        if 2 * l <= x < 3 * l and y == 0 and direction == 'T':
+            return (3 * l) - 1 - x, l, 'D'
+        # 1 bottom to 1 top
+        if x < l and y == l and direction == 'T':
+            return (3 * l) - 1 - x, 0, 'D'
+        # 2 top to bottom
+        if x == 2 * l and y < l and direction == 'L':
+            return y + l, l, 'D'
+        # 2 bottom to 2 top
+        if l <= x < 2 * l and y == l and direction == 'T':
+            return 2 * l, x - l, 'R'
+        # 3 top to 3 bottom:
+        if x == 0 and l <= y < 2 * l and direction == 'L':
+            return (5 * l) - 1 - y, gh - 1, 'T'
+        # 3 bottom to 3 top
+        if 3 * l <= x < gw and y == gh - 1 and direction == 'D':
+            return 0, (5 * l) - 1 - x, 'R'
+        # 4 top to 4 bottom
+        if x < l and y == 2 * l - 1 and direction == 'D':
+            return (3 * l) - 1 - x, gh - 1, 'T'
+        # 4 bottom to 4 top
+        if 2 * l <= x < 3 * l and y == gh - 1 and direction == 'D':
+            return (3 * l) - 1 - x, 2 * l - 1, 'T'
+        # 5 top to 5  bottom
+        if l <= x < 2 * l and y == 2 * l - 1 and direction == 'D':
+            return 2 * l, (4 * l) - 1 - x, 'R'
+        # 5 bottom to 5 top
+        if x == 2 * l and 2 * l <= y < gh and direction == 'L':
+            return (4 * l) - 1 - x, 2 * l, 'T'
+        # 6 top to 6 bottom
+        if x == 3 * l - 1 and y < l and direction == 'R':
+            return gw - 1, gh - 1 - y, 'L'
+        # 6 bottom to 6 top
+        if x == gw - 1 and 2 * l <= y < gh and direction == 'R':
+            return 3 * l - 1, gh - 1 - y, 'R'
+        # 7 top to 7 bottom
+        if x == 3 * l - 1 and l <= y < 2 * l and direction == 'R':
+            return (5 * l) - 1 - y, 2 * l, 'D'
+        # 7 bottom to 7 top
+        if 3 * l <= x < gw and y == 2 * l and direction == 'T':
+            return 3 * l - 1, l + gw - 1 - x, 'L'
+
+        dx, dy = dir_moves[direction]
+        return x + dx, y + dy, direction
+
+    def wrapCallBackFullInput(grid, gh, gw, x, y, direction):
+        l = 50
+        #     3 4
+        #   1|_|_|7
+        #   2|_|6
+        # 1|_|_|7
+        # 3|_|5
+        #   4
+
+        # 1 top to 1 bottom
+        if x == l and y < l and direction == 'L':
+            return 0, (3 * l) - 1 - y, 'R'
+        # 1 bottom to 1 top
+        if x == 0 and 2 * l <= y < 3 * l and direction == 'L':
+            return l, (3 * l) - 1 - y, 'R'
+        # 2 top to 2 bottom
+        if x == l and l <= y < 2 * l and direction == 'L':
+            return y - l, 2 * l, 'D'
+        # 2 bottom to 2 top
+        if x < l and y == 2 * l and direction == 'T':
+            return l, l + x, 'R'
+        # 3 top to 3 bottom
+        if l <= x < 2 * l and y == 0 and direction == 'T':
+            return 0, (2 * l) + x, 'R'
+        # 3 bottom to 3 top
+        if x == 0 and 3 * l <= y < gh and direction == 'L':
+            return y - (2 * l), 0, 'D'
+        # 4 top to 4 bottom
+        if 2 * l <= x < gw and y == 0 and direction == 'T':
+            return x - (2 * l), gh - 1, 'T'
+        # 4 bottom to 4 top
+        if x < l and y == gh - 1 and direction == 'D':
+            return (2 * l) + x, 0, 'D'
+        # 5 top to bottom
+        if l <= x < 2 * l and y == (3 * l) - 1 and direction == 'D':
+            return l - 1, x + (2 * l), 'L'
+        # 5 bottom to 5 top
+        if x == l - 1 and 3 * l <= y < gh and direction == 'R':
+            return y - (2 * l), (3 * l) - 1, 'T'
+        # 6 top to 6 bottom
+        if 2 * l <= x < 3 * l and y == l - 1 and direction == 'D':
+            return (2 * l) - 1, x - l, 'L'
+        # 6 bottom to top
+        if x == (2 * l) - 1 and l <= y < 2 * l and direction == 'R':
+            return y + l, l - 1, 'T'
+        # 7 top to bottom
+        if x == gw - 1 and y < l and direction == 'R':
+            return (2 * l) - 1, (3 * l) - 1 - y, 'L'
+        # 7 bottom to top
+        if x == (2 * l) - 1 and 2 * l <= y < 3 * l and direction == 'R':
+            return gw - 1, (3 * l) - 1 - y, 'L'
+
+        dx, dy = dir_moves[direction]
+        return x + dx, y + dy, direction
+
+    if len(lines) > 14: # full input
+        return move(lines, wrapCallBackFullInput)
+    return move(lines, wrapCallbackTestInput)
