3ca7df245f4bd805350b793507039508bb3d274e
Yannick <tech@yalit.be>
2023-11-01 13:31:49 +0100

    ref : refactor python library names

--- python/main.py
+++ python/main.py
@@ -13,14 +13,14 @@ day = sys.argv[3]
 
 sys.path.append(os.path.abspath(os.path.join('..')))
 
-dayScript = import_module(f'{year}.day_{day}.aoc')
+dayScript = import_module(f'aoc{year}.day_{day}.aoc')
 
 if runType == 'test':
     print(f'Running day {day} --- TEST')
-    file = open(f'../{year}/day_{day}/input_test.txt', "r")
+    file = open(f'../aoc{year}/day_{day}/input_test.txt', "r")
 else:
     print(f'Running day {day}')
-    file = open(f'../{year}/day_{day}/input.txt', "r")
+    file = open(f'../aoc{year}/day_{day}/input.txt', "r")
 
 lines = [x.strip('\n') for x in file.readlines()]
 
