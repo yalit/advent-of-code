f5e5ae5488ae429362adedcee61bb82fadc412fd
Yannick <tech@yalit.be>
2023-12-02 08:18:54 +0100

    refacto : 2023 day 2 :recycle:

--- aoc2023/day_2/aoc.py
+++ aoc2023/day_2/aoc.py
@@ -1,40 +1,33 @@
+import re
 from functools import reduce
 
+data_pattern = re.compile(r'(\d+) (red|green|blue)')
 
 def handle_part_1(lines: list[str]) -> int:
-    max_cubes = {'red' : 12, 'green': 13, 'blue': 14}
+    max_cubes = {'red': 12, 'green': 13, 'blue': 14}
     possible_games = []
 
     for line in lines:
         [game, info] = line.split(":")
-        [_, game_id] = game.split(" ")
         possible = True
-        for show in info.split(';'):
-            for data in show.split(','):
-                [n,color] = data.strip().split(' ')
-                if possible and (color not in max_cubes or  int(n) > max_cubes[color]):
-                    possible = False
+        for (n, color) in data_pattern.findall(info):
+            if possible and int(n) > max_cubes[color]:
+                possible = False
 
         if possible:
-            possible_games.append(int(game_id))
+            possible_games.append(int(game.split(" ")[1]))
+
     return sum(possible_games)
 
 
 def handle_part_2(lines: list[str]) -> int:
-    min_cubes_power = []
+    min_cubes_power = 0
 
     for line in lines:
-        [game, info] = line.split(":")
-        [_, game_id] = game.split(" ")
         min_colors = {}
-        for show in info.split(';'):
-            for data in show.split(','):
-                [n,color] = data.strip().split(' ')
-                if color not in min_colors:
-                    min_colors[color] = int(n)
-                else:
-                    min_colors[color] = max(min_colors[color], int(n))
+        for (n, color) in data_pattern.findall(line.split(":")[1]):
+            min_colors[color] = int(n) if color not in min_colors else max(min_colors[color], int(n))
 
-        min_cubes_power.append(reduce(lambda p, x: p * x, list(map(int, min_colors.values())), 1))
+        min_cubes_power += reduce(lambda p, x: p * x, list(map(int, min_colors.values())), 1)
 
-    return sum(min_cubes_power)
+    return min_cubes_power
