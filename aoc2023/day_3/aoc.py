f297db234c94c83e320869e9b99506b48790679f
Yannick <tech@yalit.be>
2023-12-03 13:16:08 +0100

    aoc : refacto 2023 day 3 :tada:

--- aoc2023/day_3/aoc.py
+++ aoc2023/day_3/aoc.py
@@ -3,32 +3,30 @@ import re
 from python.libraries.utils import neighbors
 from python.libraries.array import inbound
 
+
 def handle_part_1(lines: list[str]) -> int:
     s = 0
     m = [list(l) for l in lines]
+    sns = {}
     h = len(m)
     w = len(m[0])
 
-    for r, row in enumerate(m):
-        n = ''
-        legible = False
-        n_end = False
-        for c, char in enumerate(row):
-            if not char.isdigit() or c == w -1:
-                n_end = True
-            if char.isdigit():
-                n += char
-                for [dr, dc] in neighbors:
-                    if not inbound(r+dr, c+dc, h, w):
-                        continue
-                    if m[r+dr][c+dc] != '.' and not m[r+dr][c+dc].isdigit():
-                        legible = True
+    for r, row in enumerate(lines):
+        ns = re.finditer(r'(\d+)', row)
+        for n in ns:
+            for c in range(n.start(), n.end()):
+                sns[(r, c)] = int(n.group())
 
-            if n_end:
-                s += int(n) if n != '' and legible else 0
-                n = ''
-                legible = False
-                n_end = False
+    for r, row in enumerate(m):
+        for c, char in enumerate(row):
+            if char.isdigit() or char == '.':
+                continue
+            gs = set()
+            for [dr, dc] in neighbors:
+                if inbound(r+dr, c+dc, h, w) and lines[r+dr][c+dc].isdigit():
+                    gs.add(sns[(r+dr,c+dc)])
+            for n in gs:
+                s += n
     return s
 
 
@@ -51,8 +49,8 @@ def handle_part_2(lines: list[str]) -> int:
             gear_set = set()
             for [dr, dc] in neighbors:
                 c = gear.start()
-                if inbound(r+dr, c + dc, h, w) and (r+dr, c + dc) in sns:
-                    gear_set.add(sns[(r+dr, c + dc)])
+                if inbound(r + dr, c + dc, h, w) and (r + dr, c + dc) in sns:
+                    gear_set.add(sns[(r + dr, c + dc)])
             if len(gear_set) == 2:
                 s += list(gear_set)[0] * list(gear_set)[1]
 
