d339c56390afa7253403b7a0b03f07d35ecc3449
Yannick <3314374+yalit@users.noreply.github.com>
2023-12-04 09:18:38 +0000

    aoc : refacto 2023 day 4 :tada:

--- aoc2023/day_4/aoc.py
+++ aoc2023/day_4/aoc.py
@@ -4,16 +4,14 @@ from functools import reduce
 def handle_part_1(lines: list[str]) -> int:
     s = 0
     for line in lines:
-        good = [int(x) for x in re.findall(r'(\d+)',line.split(": ")[1].split(" | ")[0])]
-        tomatch = [int(n) for n in (re.findall(r'(\d+)',line.split(": ")[1].split(" | ")[1]))]
+        cards = line.split(": ")[1].split(" | ")
+        good = [int(x) for x in re.findall(r'(\d+)',cards[0])]
+        tomatch = [int(n) for n in re.findall(r'(\d+)',cards[1])]
         st = 0
-        n = 1
+        n = 0
         for m in tomatch:
             if m in good:
-                if n == 1:
-                    st += 1
-                if n > 1:
-                    st *= 2
+                st = st + 1 if n == 0 else st * 2
                 n += 1
         s += st
 
@@ -21,11 +19,12 @@ def handle_part_1(lines: list[str]) -> int:
 
 
 def handle_part_2(lines: list[str]) -> int:
-    cards = [1 for _ in lines]
+    cardNumbers = [1 for _ in lines]
 
     for i, line in enumerate(lines):
-        good = [int(x) for x in re.findall(r'(\d+)',line.split(": ")[1].split(" | ")[0])]
-        tomatch = [int(n) for n in (re.findall(r'(\d+)',line.split(": ")[1].split(" | ")[1]))]
+        cards = line.split(": ")[1].split(" | ")
+        good = [int(x) for x in re.findall(r'(\d+)',cards[0])]
+        tomatch = [int(n) for n in re.findall(r'(\d+)',cards[1])]
         n = 0
         for m in tomatch:
             if m in good:
@@ -33,7 +32,6 @@ def handle_part_2(lines: list[str]) -> int:
                 
         if n > 0:
             for a in range(1,n + 1):
-                cards[i+a] = cards[i+a] + cards[i]
-        print(i+1, n, cards)
-    print(cards)
-    return sum(cards)
+                cardNumbers[i+a] = cardNumbers[i+a] + cardNumbers[i]
+
+    return sum(cardNumbers)
