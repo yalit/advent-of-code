67decb7f94b056afd16f9428c5a3ec4b3a4bc030
Yannick <tech@yalit.be>
2023-12-05 22:09:25 +0100

    aoc : 2015 day 17

--- 2015/src/handle_d17.ts
+++ 2015/src/handle_d17.ts
@@ -1,42 +1,30 @@
-let allCombinations = []
-
-function getAllCombinations(liters: number, containers: Array<number>, combination: Array<number>) {
-    if (liters === 0) {
-        if (allCombinations.indexOf(combination.join('-')) === -1) {
-            allCombinations.push(combination.join('-'))
-        }
-        return
-    }
-
-    if (containers.length === 0) {
-        return   
-    }
-
-    for (let i = 0; i < containers.length; i++) {
-        let newContainers = containers.slice(i+1)
-        if (liters < containers[i]) {
-            getAllCombinations(liters, newContainers, combination)
-        } else {
-            let newCombination = [...combination]
-            newCombination.push(containers[i])
-            getAllCombinations(liters-containers[i], newContainers, newCombination)
-        }
-    }
-}
-
 function handleInput_1(lines: Array<string>){
-    const containers = lines.map(line => parseInt(line)).sort((a,b) => b-a)
-    getAllCombinations(150,containers,[])
-    console.log(allCombinations);
-    
-    return allCombinations.length
+    let data: number[] = []
+    lines.forEach((line) => {data.push(+line) })
+
+    let comb = combinations(data, 150)
+
+    return comb.length
 }
 
 function handleInput_2(lines: Array<string>){
-    return 0
+    let data: number[] = []
+    lines.forEach((line) => {data.push(+line) })
+
+    let comb = new Set(combinations(data, 150))
+    let min = Infinity
+
+    comb.forEach(s => min = s.length < min ? s.length : min)
+    return Array.from(comb).filter(a => a.length == min).length
 }
 
 
 export function handleInput(lines: Array<string>) {
     return [handleInput_1(lines), handleInput_2(lines)]
+}
+
+function combinations(array, sum) {
+  return new Array(1 << array.length).fill("").map(
+    (e1, i) => array.filter((e2, j) => i & 1 << j))
+      .filter(a => a.reduce((s,a) => s +a, 0) === sum);
 }
