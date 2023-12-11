6862fbcceafe2f016387fbbbd524c782d58316b0
Yannick <tech@yalit.be>
2023-12-11 09:44:07 +0100

    refactor : refactor 2023 day 11 with python syntactic sugar

--- aoc2023/day_11/aoc.py
+++ aoc2023/day_11/aoc.py
@@ -1,45 +1,25 @@
-import re
 from python.libraries.array import transpose
 
 def handle_part_1(lines: list[str]) -> int:
     return get_min_dist(lines, 1)
 
-
 def handle_part_2(lines: list[str]) -> int:
     return get_min_dist(lines, 1000000-1)
 
 def get_min_dist(lines, additional):
-    galaxies = []
-
-    empty_rows = []
-    empty_cols = []
-    for r, row in enumerate(lines):
-        if all([x == '.' for x in row]):
-            empty_rows.append(r)
-
-    transposed = transpose(lines)
-    for r, row in enumerate(transposed):
-        if all([x == '.' for x in row]):
-            empty_cols.append(r)
-
-    for r, row in enumerate(lines):
-        f = list(re.finditer("\#", row))
-        for m in f:
-            galaxies.append((r, m.start()))
+    empty_rows = [r for r, row in enumerate(lines) if all([x == '.' for x in row])]
+    empty_cols = [c for c, col in enumerate(transpose(lines)) if all([x == '.' for x in col])]
+    galaxies = [(r,c) for r, row in enumerate(lines) for c, col in enumerate(row) if col == '#']
 
     total = 0
-    for i, g in enumerate(galaxies):
-        for tg in galaxies[i + 1:]:
-            min_dist = abs(tg[0] - g[0]) + abs(tg[1] - g[1])
-            x = sorted((tg[0], g[0]))
-            y = sorted((tg[1], g[1]))
-
+    for i, (r1, r2) in enumerate(galaxies):
+        for (t1, t2) in galaxies[i + 1:]:
+            total += abs(t1 - r1) + abs(t2 - r2)
             for r in empty_rows:
-                if x[0] < r < x[1]:
-                    min_dist += additional
+                if min(r1, t1) < r < max(r1, t1):
+                    total += additional
             for c in empty_cols:
-                if y[0] < c < y[1]:
-                    min_dist += additional
-            total += min_dist
+                if min(r2, t2) < c < max(r2, t2):
+                    total += additional
 
     return total
