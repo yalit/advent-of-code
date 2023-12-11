1086064e886043a6ffb5499ff718f24771e69d7c
Yannick <tech@yalit.be>
2023-12-11 16:36:45 +0100

    refactor : clean the 2023 day 10

--- aoc2023/day_10/aoc.py
+++ aoc2023/day_10/aoc.py
@@ -13,45 +13,23 @@ connections = {
 def handle_part_1(lines: list[str]) -> int:
     s = get_s(lines)
     st = get_shape_of_s(lines, s)
+    lines = [list(l) for l in lines]
+    lines[s[0]][s[1]] = st
 
-    m_steps = 0
-    visited = set()
-    visited.add(s)
-    to_visit = [(s[0] + dr, s[1] + dc) for dr, dc in connections[st]]
-    while len(to_visit) > 0:
-        nexts = []
-        for r, c in to_visit:
-            visited.add((r, c))
-            for dr, dc in connections[lines[r][c]]:
-                if not (r + dr, c + dc) in visited:
-                    nexts.append((r + dr, c + dc))
-
-        to_visit = nexts
-        m_steps += 1
-    return m_steps
+    loop = get_loop(lines, s, st)
+    return int(len(loop) / 2)
 
 
 def handle_part_2(lines: list[str]) -> int:
     s = get_s(lines)
     st = get_shape_of_s(lines, s)
     lines = [list(l) for l in lines]
+    lines[s[0]][s[1]] = st
+
     h = len(lines)
     w = len(lines[0])
 
-    loop = set()
-    loop.add(s)
-    to_visit = [(s[0] + dr, s[1] + dc) for dr, dc in connections[st]]
-    while len(to_visit) > 0:
-        nexts = []
-        for r, c in to_visit:
-            loop.add((r, c))
-            for dr, dc in connections[lines[r][c]]:
-                if not (r + dr, c + dc) in loop:
-                    nexts.append((r + dr, c + dc))
-
-        to_visit = nexts
-
-    lines[s[0]][s[1]] = st
+    loop = get_loop(lines, s, st)
     for r, row in enumerate(lines):
         for c, col in enumerate(row):
             if (r, c) in loop:
@@ -93,26 +71,7 @@ def handle_part_2(lines: list[str]) -> int:
             if crossings % 2 == 1:
                 inside.add((r, c))
 
-    m = {
-        '|': '┃',
-        '-': '━',
-        '7': '┓',
-        'F': '┏',
-        'L': '┗',
-        'J': '┛',
-    }
-    for r, row, in enumerate(lines):
-        for c, col in enumerate(row):
-            if (r, c) in inside:
-                lines[r][c] = '\x1b[92m+\x1b[39m'
-                continue
-            if (r, c) in loop:
-                lines[r][c] = m[col]
-                continue
-            lines[r][c] = '-'
-
-    for row in lines:
-        print("".join(row))
+    visualize(lines, inside, loop)
 
     return len(inside)
 
@@ -144,3 +103,41 @@ def get_shape_of_s(lines, s):
             st = tube
             break
     return st
+
+
+def get_loop(lines, start, start_shape):
+    visited = set()
+    to_visit = [start]
+    while len(to_visit) > 0:
+        nexts = []
+        for r, c in to_visit:
+            visited.add((r, c))
+            for dr, dc in connections[lines[r][c]]:
+                if not (r + dr, c + dc) in visited:
+                    nexts.append((r + dr, c + dc))
+
+        to_visit = nexts
+
+    return visited
+
+def visualize(lines, inside, loop):
+    m = {
+        '|': '┃',
+        '-': '━',
+        '7': '┓',
+        'F': '┏',
+        'L': '┗',
+        'J': '┛',
+    }
+    for r, row, in enumerate(lines):
+        for c, col in enumerate(row):
+            if (r, c) in inside:
+                lines[r][c] = '\x1b[92m+\x1b[39m'
+                continue
+            if (r, c) in loop:
+                lines[r][c] = m[col]
+                continue
+            lines[r][c] = '-'
+
+    for row in lines:
+        print("".join(row))
