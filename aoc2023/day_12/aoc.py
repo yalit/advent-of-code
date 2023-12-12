27e96ae9bfff49d85f61f7ee26bcee6b236fb9fe
Yannick <tech@yalit.be>
2023-12-13 00:44:48 +0100

    aoc : 2023 day 12 finished

--- aoc2023/day_12/aoc.py
+++ aoc2023/day_12/aoc.py
@@ -1,63 +1,41 @@
-import re
-
 def handle_part_1(lines: list[str]) -> int:
    total = 0
-   for line in lines[-1:]:
-       add = get_nb_assignments(*line.split())
-       print("line nb : ", add)
-       total += get_nb_assignments(*line.split())
+   for i, line in enumerate(lines):
+       springs, conditions = line.split()
+       conditions = tuple([int(n) for n in conditions.split(',')])
+       total += get_nb_arrangements_rec(springs, conditions)
    return total
 
 
 def handle_part_2(lines: list[str]) -> int:
-    return 0
+   total = 0
+   for i, line in enumerate(lines):
+       springs, conditions = line.split()
+       springs = "?".join([springs for _ in range(5)])
+       conditions = ",".join([conditions for _ in range(5)])
+       conditions = tuple([int(n) for n in conditions.split(',')])
 
-def get_nb_assignments(springs, conditions):
+       total += get_nb_arrangements_rec(springs, conditions)
+   return total
+
+cache = {}
+def get_nb_arrangements_rec(springs, conditions):
+    if (springs, conditions) in cache:
+        return cache[(springs, conditions)]
+
+    if conditions == ():
+        return 1 if springs.count('#') == 0 else 0
+
+    if springs == "":
+        return 1 if conditions == () else 0
+
     nb = 0
+    if springs[0] == '.' or springs[0] == '?':
+        nb += get_nb_arrangements_rec(springs[1:], conditions)
 
-    to_test = [(0, [x for x in springs.split('.') if len(x) > 0], [int(n) for n in conditions.split(',')])]
-    
-    while to_test:
-        cur_step, spring, condition = to_test.pop()
-        print(cur_step, spring, condition, nb)
-        if len(condition) == 0:
-            if len(spring) == 0:
-                nb += 1
-            continue
-        if len(spring) == 0:
-            continue
-        
-        s = spring[0]
-        c = condition[0]
+    if springs[0] == '#' or springs[0] == '?':
+        if len(springs) >= conditions[0] and springs[:conditions[0]].count('.') == 0 and (len(springs) == conditions[0] or springs[conditions[0]] != '#'):
+            nb += get_nb_arrangements_rec(springs[conditions[0] + 1:], conditions[1:])
 
-        if len(s) < c:
-            print("spring not long enough")
-            to_test.append((0,spring[1:], condition))
-            continue
-
-        if all([x == '#' for x in s[:c]]):
-            if len(s) == c:
-                print("found a full spring")
-                to_test.append((0,spring[1:], condition[1:]))
-            elif s[c] != '#':
-                print("found a part of a spring")
-                to_test.append((0,[s[c+1:]] + spring[1:], condition[1:]))
-            continue
-
-        if s[cur_step] == '#':
-            print("testing the next part of the spring")
-            to_test.append((cur_step+1, spring, condition))
-            continue
-
-        # can only use . on the first step if not finished
-        if cur_step == 0 and len(s) > c:
-            print("beginning of the spring, testing a '.'")
-            to_test.append((0, [s[cur_step+1:]] + spring[1:], condition))
-        
-        # test '#'
-        print("testing a '#'")
-        s = s[:cur_step] + '#' + s[cur_step+1:]
-        to_test.append((cur_step+1, [s]+spring[1:], condition))
-        
-    
+    cache[(springs, conditions)] = nb
     return nb
