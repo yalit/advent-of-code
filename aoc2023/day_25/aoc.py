2214cea8ea73dfd2ad4e8b9919f618a72a29a58f
Yannick <tech@yalit.be>
2023-12-25 21:21:06 +0100

    aoc : 2023 day 25 :itschristmas:

--- aoc2023/day_25/aoc.py
+++ aoc2023/day_25/aoc.py
@@ -1,5 +1,38 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def handle_part_1(lines: list[str]) -> int:
-    return 0
+    g = nx.Graph()
+    graph = {}
+    for line in lines:
+        f, to = line.split(': ')
+        if f not in graph:
+            graph[f] = set()
+            g.add_node(f)
+
+        for n in to.split():
+            if n not in graph:
+                graph[n] = set()
+                g.add_node(n)
+            graph[f].add(n)
+            g.add_edge(f, n)
+            graph[n].add(f)
+
+    cuts = nx.minimum_edge_cut(g)
+    for f,t in cuts:
+        graph[f].remove(t)
+        graph[t].remove(f)
+
+    to_visit = [list(graph.keys())[0]]
+    seen = set()
+    while to_visit:
+        n = to_visit.pop()
+        seen.add(n)
+        for neighbor in graph[n]:
+            if neighbor not in seen:
+                to_visit.append(neighbor)
+
+    return (len(graph.keys()) - len(seen)) * len(seen)
 
 
 def handle_part_2(lines: list[str]) -> int:
