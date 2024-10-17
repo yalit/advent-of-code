918ff146cf35f8a52d42b08077d70454a7807918
Yannick <tech@yalit.be>
2024-10-17 20:35:41 +0200

    aoc : 2019 day 12 part 2

--- aoc2019/day_12/aoc.py
+++ aoc2019/day_12/aoc.py
@@ -1,4 +1,7 @@
-from typing import Tuple
+import math
+from typing import Tuple, List
+
+from sympy.physics.units import velocity
 
 
 def get_planets(lines: list[str])->list[Tuple[int, int, int]]:
@@ -43,5 +46,48 @@ def handle_part_1(lines: list[str]) -> int:
     return sum([p*v for p,v in zip(potential, kinetic)])
 
 
+def get_next_position(planets: List[int], velocity: List[int]) -> Tuple[List[int], List[int]]:
+    new_planets = planets[:]
+    new_velocities = velocity[:]
+    for a in range(len(planets)):
+        for b in range(len(planets)):
+            if a == b:
+                continue
+
+            if planets[a] < planets[b]:
+                new_velocities[a] += 1
+            elif planets[a] > planets[b]:
+                new_velocities[a] -= 1
+        new_planets[a] += new_velocities[a]
+
+    return new_planets, new_velocities
+
 def handle_part_2(lines: list[str]) -> int:
-    return 0
+    planets = get_planets(lines)
+    original_planet_x, planet_x = [p[0] for p in planets], [p[0] for p in planets]
+    original_planet_y, planet_y = [p[1] for p in planets], [p[1] for p in planets]
+    original_planet_z, planet_z = [p[2] for p in planets], [p[2] for p in planets]
+
+    original_velocity_x, velocity_x = [0 for _ in planets], [0 for _ in planets]
+    original_velocity_y, velocity_y = [0 for _ in planets], [0 for _ in planets]
+    original_velocity_z, velocity_z = [0 for _ in planets], [0 for _ in planets]
+
+    step_x, step_y, step_z = 1, 1, 1
+
+    planet_x, velocity_x = get_next_position(planet_x, velocity_x)
+    planet_y, velocity_y = get_next_position(planet_y, velocity_y)
+    planet_z, velocity_z = get_next_position(planet_z, velocity_z)
+
+    while not (planet_x == original_planet_x and velocity_x == original_velocity_x):
+        planet_x, velocity_x = get_next_position(planet_x, velocity_x)
+        step_x += 1
+
+    while not (planet_y == original_planet_y and velocity_y == original_velocity_y):
+        planet_y, velocity_y = get_next_position(planet_y, velocity_y)
+        step_y += 1
+
+    while not (planet_z == original_planet_z and velocity_z == original_velocity_z):
+        planet_z, velocity_z = get_next_position(planet_z, velocity_z)
+        step_z += 1
+
+    return math.lcm(step_x, math.lcm(step_y, step_z))
