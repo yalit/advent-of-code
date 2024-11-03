bfde2da912d31ae66d4464a2c84c11aeda31d770
Yannick <tech@yalit.be>
2024-11-03 09:17:54 +0100

    aoc : 2019 day 7 part 2

--- aoc2019/day_7/aoc.py
+++ aoc2019/day_7/aoc.py
@@ -1,16 +1,17 @@
-from python.libraries.intCode.intCodeRunner import IntCodeRunner
 from itertools import permutations
+from python.libraries.intCode.intCodeComputer import IntCodeComputer
+
 
 def handle_part_1(lines: list[str]) -> int:
     program = list(map(int, lines[0].split(',')))
 
-    runner = IntCodeRunner(program)
-
     max_signal = 0
     for a in permutations(range(5)):
+        computers = [IntCodeComputer(program) for _ in range(5)]
         output = 0
-        for inp in a:
-            output = runner.execute(program_inputs=[inp, output])
+        for i, inp in enumerate(a):
+            computers[i].set_inputs([inp, output])
+            output = computers[i].execute()[-1]
 
         max_signal = max(max_signal, output)
 
@@ -18,4 +19,21 @@ def handle_part_1(lines: list[str]) -> int:
 
 
 def handle_part_2(lines: list[str]) -> int:
-    return 0
+    program = list(map(int, lines[0].split(',')))
+
+    max_signal = 0
+    for a in permutations(range(5, 10)):
+        computers = [IntCodeComputer(program) for _ in range(5)]
+        output = 0
+        # Set phase settings
+        for i, inp in enumerate(a):
+            computers[i].set_inputs([inp, output])
+            output = computers[i].execute()[-1]
+        while not computers[4].end:
+            for i in range(5):
+                computers[i].set_inputs([output])
+                output = computers[i].execute()[-1]
+
+        max_signal = max(max_signal, output)
+
+    return max_signal
