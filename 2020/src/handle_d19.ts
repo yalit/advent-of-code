b5b5103e0e69243f1ac871138d3b09a9756fb9c1
Yannick <tech@yalit.be>
2024-11-08 09:43:06 +0100

    aoc : 2020 day 19

--- 2020/src/handle_d19.ts
+++ 2020/src/handle_d19.ts
@@ -1,90 +1,70 @@
-import {isText} from "./libraries/types";
-
-const getRules = (lines: Array<string>) => {
-    const rules = {}
-
-    const getRuleDetails = (rule: string) => {
-        if (rule[0] === '"') {
-            return rule[1]
-        } else {
-            return parseInt(rule)
-        }
+const get_Rules = (lines: Array<string>) => {
+    const rules = new Map()
+    let i = 0
+    while (lines[i] !== '') {
+        const [key, value] = lines[i].split(': ')
+        rules.set(key, value)
+        i++
     }
-
-    lines.forEach(line => {
-        const [key, value] = line.split(': ')
-        const elements = value.split(' | ').map(v => v.split(' ').map(getRuleDetails))
-        rules[parseInt(key)] = elements
-    })
     return rules
 }
 
-const matchMessage = (message: string, rules: any, rule: Array<number|string>, position: number = 0) => {
-    if (position === message.length) {
-        return true
+const get_regex = (rules: Map<string, string>, key: string, full: boolean = true): RegExp => {
+    let re = ''
+
+    const get_rule = (key: string): string => {
+        const rule = rules.get(key)
+        if (rule.match(/"[a-z]"/)) {
+            return rule[1]
+        }
+        return `(${rule.split(' | ').map(r => r.split(' ').map(k => get_rule(k)).join('')).join('|')})`
     }
 
-    if (rule.length === 0) {
-        return false
-    }
-
-    let valid = false
-    const element = rule[0]
-    if (isText(element)){
-        if (element === message[position]) {
-            valid = matchMessage(message, rules, rule.slice(1), position+1)
-        } else{
-            valid = false
-        }
-    } else {
-        const newRules = rules[element]
-        for (let newRule of newRules) {
-            valid = matchMessage(message, rules, newRule.concat(rule.slice(1)), position)
-            if (valid) {
-                break
-            }
-        }
-    }
-    return valid
+    return new RegExp(`${full ? '^' : ''}${get_rule(key)}${full ? '$' : ''}`)
 }
-
 
 function handleInput_1(lines: Array<string>){
     const split = lines.indexOf('')
-    const rules = getRules(lines.slice(0, split))
+    const rules = get_Rules(lines)
 
-    const messages = lines.slice(split+1)
-    return messages.filter(m => matchMessage(m, rules, rules[0][0])).length
+    const regex = get_regex(rules, '0')
+    return lines.slice(split + 1).filter(l => regex.test(l)).length
 }
 
 function handleInput_2(lines: Array<string>){
     const split = lines.indexOf('')
-    const rules = getRules(lines.slice(0, split))
-    const messages = lines.slice(split+1)
+    const rules = get_Rules(lines)
 
-    rules[8] = [[42], [42, 8]]
-    rules[11] = [[42, 31], [42, 11, 31]]
+    // change is that rule 8 is now 42 | 42 8 and rule 11 is 42 31 | 42 11 31
+    // this means that rule 0 is now 8 11
+    rules.set('8', '42 | 42 8')
+    rules.set('11', '42 31 | 42 11 31')
 
-    console.log(rules)
+    // so it means that we always need at least 2 42's and 1 31 timed n times
+    // and that you'll have all 42 at the beginning and all 31 at the end
+    // so let's compute the regex for 42 and 31
+    const regex_42 = get_regex(rules, '42', false)
+    const regex_31 = get_regex(rules, '31', false)
 
-    // const tobefound = [
-    //     'bbabbbbaabaabba',
-    //     'babbbbaabbbbbabbbbbbaabaaabaaa',
-    //     'aaabbbbbbaaaabaababaabababbabaaabbababababaaa',
-    //     'bbbbbbbaaaabbbbaaabbabaaa',
-    //     'bbbababbbbaaaaaaaabbababaaababaabab',
-    //     'ababaaaaaabaaab',
-    //     'ababaaaaabbbaba',
-    //     'baabbaaaabbaaaababbaababb',
-    //     'abbbbabbbbaaaababbbbbbaaaababb',
-    //     'aaaaabbaabaaaaababaa',
-    //     'aaaabbaabbaaaaaaabbbabbbaaabbaabaaa',
-    //     'aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba'
-    // ]
-    // console.log(messages.filter(m => matchMessage(m, rules, rules[0][0])).filter(m => !tobefound.includes(m)))
-    return messages.filter(m => matchMessage(m, rules, rules[0][0])).length
+    return lines.slice(split + 1).filter(l => {
+        let n42 = 0
+        let n31 = 0
+        let i = 0
+        let m = l.match(regex_42)
+        while (m && m.index === 0) {
+            n42++
+            i += m[0].length
+            m = l.slice(i).match(regex_42)
+        }
+        m = l.slice(i).match(regex_31)
+        while (m && m.index === 0) {
+            n31++
+            i += m[0].length
+            m = l.slice(i).match(regex_31)
+        }
+        return n42 > n31 && n31 > 0 && i === l.length
+    }).length
 }
-
 
 export function handleInput(lines: Array<string>) {
     return [handleInput_1(lines), handleInput_2(lines)]
