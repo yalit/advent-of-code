ef7af9ae5f0cd4a1314082e73a6ec29e19cfe3c9
Yannick <tech@yalit.be>
2024-11-13 11:51:11 +0100

    aoc : 2020 day 20 (all finished!!!) :tada:

--- 2020/src/handle_d20.ts
+++ 2020/src/handle_d20.ts
@@ -3,6 +3,7 @@
  * Each tile has neighbors
  * the outer edges of the pattern are unique
  */
+import { flipStringArray, rotateStringArray } from "./libraries/array";
 
 type Edges = {
   top: string;
@@ -59,17 +60,7 @@ const getReversedEdges = (edges: Edges): Edges => {
 };
 
 const rotateTile = (tile: Tile): Tile => {
-  let newlines = [];
-
-  for (let i = tile.lines.length - 1; i >= 0; i--) {
-    newlines.push(tile.lines.map((l) => l[i]).join(""));
-  }
-
-  return { id: tile.id, lines: newlines, edges: getEdges(newlines) };
-};
-
-const flipTile = (tile: Tile): Tile => {
-  const newLines = tile.lines.map((l) => l.split("").reverse().join(""));
+  const newLines = rotateStringArray(tile.lines);
   return {
     id: tile.id,
     lines: newLines,
@@ -77,12 +68,37 @@ const flipTile = (tile: Tile): Tile => {
   };
 };
 
-const isMatchingRight = (from: Tile, to: Tile): boolean => {
-  return from.edges.right === to.edges.left;
+const nextRotatedEdge = (e: EdgeSide): EdgeSide => {
+  const n: { [k in EdgeSide]: EdgeSide } = {
+    top: "left",
+    right: "top",
+    bottom: "right",
+    left: "bottom",
+  };
+  return n[e];
 };
 
-const isMatchingBottom = (from: Tile, to: Tile): boolean => {
-  return from.edges.bottom === to.edges.top;
+const flipTile = (tile: Tile): Tile => {
+  const newLines = flipStringArray(tile.lines);
+  return {
+    id: tile.id,
+    lines: newLines,
+    edges: getEdges(newLines),
+  };
+};
+
+const allPossibleTransformations = (tile: Tile): Array<Tile> => {
+  const flipped = flipTile(tile);
+  return [
+    tile,
+    rotateTile(tile),
+    rotateTile(rotateTile(tile)),
+    rotateTile(rotateTile(rotateTile(tile))),
+    flipped,
+    rotateTile(flipped),
+    rotateTile(rotateTile(flipped)),
+    rotateTile(rotateTile(rotateTile(flipped))),
+  ];
 };
 
 function handleInput_1(lines: Array<string>) {
@@ -111,6 +127,53 @@ function handleInput_1(lines: Array<string>) {
     .reduce((acc, tile) => acc * parseInt(tile.id), 1);
 }
 
+const findMatchingEdges = (tiles: Array<Tile>, tile: Tile): Set<EdgeSide> => {
+  let matching: Set<EdgeSide> = new Set();
+
+  for (let j in tiles) {
+    const otherTile = tiles[j];
+
+    if (tile.id === otherTile.id) {
+      continue;
+    }
+    Object.entries(tile.edges).forEach(([e, edge]: [EdgeSide, string]) => {
+      if (Object.values(otherTile.edges).includes(edge)) {
+        matching.add(e);
+      }
+      if (Object.values(getReversedEdges(otherTile.edges)).includes(edge)) {
+        matching.add(e);
+      }
+    });
+  }
+
+  return matching;
+};
+
+const getTopLeft = (tiles: Array<Tile>): Tile => {
+  for (let i in tiles) {
+    let tile = tiles[i];
+
+    const matching = findMatchingEdges(tiles, tile);
+
+    if (matching.size === 2) {
+      let m: [EdgeSide, EdgeSide] = Array.from(matching).sort() as [
+        EdgeSide,
+        EdgeSide,
+      ];
+
+      while (!(m[0] === "bottom" && m[1] === "right")) {
+        tile = rotateTile(tile);
+        m = [nextRotatedEdge(m[0]), nextRotatedEdge(m[1])].sort() as [
+          EdgeSide,
+          EdgeSide,
+        ];
+      }
+      return tile;
+    }
+  }
+};
+
+// inspired by Anthonywritescode : https://github.com/anthonywritescode/aoc2020/blob/master/day20/part2.py
 function handleInput_2(lines: Array<string>) {
   const tiles = getTiles(lines);
 
@@ -126,45 +189,164 @@ function handleInput_2(lines: Array<string>) {
     }
   });
 
+  let connections = { 2: [], 3: [], 4: [] };
+
+  tiles.forEach((tile) => {
+    connections[findMatchingEdges(tiles, tile).size].push(tile);
+  });
+
   const gridSize = Math.sqrt(tiles.length);
   let grid: Array<Array<Tile>> = [];
-  for (let a = 0; a < gridSize; a++) {
-    grid.push([]);
-    for (let b = 0; b < gridSize; b++) {
-      grid[a].push(null);
+
+  const topLeft = getTopLeft(tiles);
+  const placed = new Set();
+  for (let i = 0; i < gridSize; i++) {
+    let row: Array<Tile> = [];
+
+    // get first piece of line
+    let targetConnections = i === 0 || i === gridSize - 1 ? 2 : 3;
+    if (i === 0) {
+      row.push(topLeft);
+      placed.add(topLeft.id);
+    } else {
+      const targetEdge = grid[i - 1][0].edges.bottom;
+      for (let t in connections[targetConnections]) {
+        const borderTile = connections[targetConnections][t];
+
+        if (placed.has(borderTile.id)) {
+          continue;
+        }
+
+        if (
+          Object.values(borderTile.edges).includes(targetEdge) ||
+          Object.values(getReversedEdges(borderTile.edges)).includes(targetEdge)
+        ) {
+          const possibilites = allPossibleTransformations(borderTile);
+          for (let p in possibilites) {
+            const transformedBorderTile = possibilites[p];
+            if (targetEdge === transformedBorderTile.edges.top) {
+              row.push(transformedBorderTile);
+              placed.add(transformedBorderTile.id);
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    // find row
+    for (let j = 1; j < gridSize; j++) {
+      targetConnections =
+        i === 0 || i === gridSize - 1
+          ? j === gridSize - 1
+            ? 2
+            : 3
+          : j == gridSize - 1
+            ? 3
+            : 4;
+
+      const targetEdge = row[j - 1].edges.right;
+      for (let t in connections[targetConnections]) {
+        const nextTile = connections[targetConnections][t];
+
+        if (placed.has(nextTile.id)) {
+          continue;
+        }
+
+        if (
+          Object.values(nextTile.edges).includes(targetEdge) ||
+          Object.values(getReversedEdges(nextTile.edges)).includes(targetEdge)
+        ) {
+          const possibilites = allPossibleTransformations(nextTile);
+          for (let p in possibilites) {
+            const transformedBorderTile = possibilites[p];
+            if (targetEdge === transformedBorderTile.edges.left) {
+              row.push(transformedBorderTile);
+              placed.add(transformedBorderTile.id);
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    grid.push(row);
+  }
+
+  let innerGrid = [];
+  let totalCountElements = 0;
+  const seaMonster = [
+    [1, -18],
+    [1, -13],
+    [1, -12],
+    [1, -7],
+    [1, -6],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [2, -17],
+    [2, -14],
+    [2, -11],
+    [2, -8],
+    [2, -5],
+    [2, -2],
+  ];
+
+  const tileSize = grid[0][0].lines.length;
+  grid.forEach((row) => {
+    for (let i = 1; i < tileSize - 1; i++) {
+      let innerRow = "";
+      row.forEach((t) => {
+        const content = t.lines[i].slice(1, tileSize - 1);
+        innerRow += content;
+        totalCountElements += Array.from(content).filter(
+          (c) => c === "#",
+        ).length;
+      });
+      innerGrid.push(innerRow);
+    }
+  });
+
+  const findNbMonsters = (g: Array<string>): number => {
+    let nb = 0;
+    for (let r = 0; r < g.length - 3; r++) {
+      for (let c = 18; c < g[0].length - 2; c++) {
+        if (g[r][c] !== "#") {
+          continue;
+        }
+        const isAMonster =
+          seaMonster.filter(([dr, dc]) => g[r + dr][c + dc] === "#").length ===
+          seaMonster.length;
+
+        nb += isAMonster ? 1 : 0;
+      }
+    }
+    return nb;
+  };
+
+  const flippedInnerGrid = flipStringArray(innerGrid);
+  const allPossibleInnerGrids = [
+    innerGrid,
+    rotateStringArray(innerGrid),
+    rotateStringArray(rotateStringArray(innerGrid)),
+    rotateStringArray(rotateStringArray(rotateStringArray(innerGrid))),
+    flippedInnerGrid,
+    rotateStringArray(flippedInnerGrid),
+    rotateStringArray(rotateStringArray(flippedInnerGrid)),
+    rotateStringArray(rotateStringArray(rotateStringArray(flippedInnerGrid))),
+  ];
+
+  for (let i in allPossibleInnerGrids) {
+    const g = allPossibleInnerGrids[i];
+
+    const nbMonsters = findNbMonsters(g);
+
+    if (nbMonsters > 0) {
+      return totalCountElements - nbMonsters * (seaMonster.length + 1);
     }
   }
 
-  const searchGrid = (r: number, c: number, visited: Set<string>) => {
-    if (r === gridSize) {
-      return;
-    }
-
-    visited.forEach((v) => console.log(v));
-    allTiles.forEach((tile: Tile) => {
-      if (visited.has(tile.id)) {
-        return;
-      }
-
-      if (c > 0 && !isMatchingRight(grid[r][c - 1], tile)) return;
-      if (r > 0 && !isMatchingBottom(grid[r - 1][c], tile)) return;
-
-      grid[r][c] = tile;
-      const new_visited = new Set(visited);
-      new_visited.add(tile.id);
-      if (c === gridSize - 1) {
-        searchGrid(r + 1, 0, new_visited);
-      } else {
-        searchGrid(r, c + 1, new_visited);
-      }
-    });
-  };
-
-  searchGrid(0, 0, new Set());
-
-  console.log(grid);
-
-  return 0;
+  return "Not found";
 }
 
 export function handleInput(lines: Array<string>) {
--- 2020/src/libraries/array.ts
+++ 2020/src/libraries/array.ts
@@ -1,16 +1,30 @@
 export function arraySum(arr: Array<number>): number {
-    return arr.reduce((s: number, n: number) => s + n, 0)
+  return arr.reduce((s: number, n: number) => s + n, 0);
 }
 
 export const transpose = (matrix) => {
-    let [row] = matrix
-    return row.map((_, column) => matrix.map(row => row[column]))
-}
+  let [row] = matrix;
+  return row.map((_, column) => matrix.map((row) => row[column]));
+};
 
-export function intersection<T> (arrA: Array<T>, arrB: Array<T>): Array<T> {
-    return arrA.filter((t: T) => arrB.includes(t))
+export function intersection<T>(arrA: Array<T>, arrB: Array<T>): Array<T> {
+  return arrA.filter((t: T) => arrB.includes(t));
 }
 
 export function removeElement<T>(arr: Array<T>, elem: T): Array<T> {
-    return arr.filter(t => t !== elem)
+  return arr.filter((t) => t !== elem);
+}
+
+export function rotateStringArray(a: Array<string>): Array<string> {
+  let newArray = [];
+
+  for (let i = a[0].length - 1; i >= 0; i--) {
+    newArray.push(a.map((r) => r[i]).join(""));
+  }
+
+  return newArray;
+}
+
+export function flipStringArray(a: Array<string>): Array<string> {
+  return a.map((r) => r.split("").reverse().join(""));
 }
