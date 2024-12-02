94ed5eeeb3518887c8ede8084f555d873a959759
Yannick <3314374+yalit@users.noreply.github.com>
2024-12-02 07:42:19 +0000

    refactor: refactor day 2 with any for part 2

--- aoc2024/day_2/aoc.py
+++ aoc2024/day_2/aoc.py
@@ -1,4 +1,5 @@
-def is_safe(deltas):
+def is_safe(line):
+    deltas = [a-b for a,b in zip(line, line[1:])]
     return (
         min(list(map(abs, deltas))) >= 1
         and max(list(map(abs, deltas))) <= 3
@@ -11,8 +12,7 @@ def handle_part_1(lines: list[str]) -> int:
 
     for line in lines:
         ns = list(map(int, line.split()))
-        deltas = [a - b for a, b in zip(ns[:-1], ns[1:])]
-        safe += 1 if is_safe(deltas) else 0
+        safe += 1 if is_safe(ns) else 0
     return safe
 
 
@@ -21,15 +21,7 @@ def handle_part_2(lines: list[str]) -> int:
 
     for line in lines:
         ns = list(map(int, line.split()))
-        deltas = [a - b for a, b in zip(ns[:-1], ns[1:])]
-        if is_safe(deltas):
+        if any(is_safe(ns[:i]+ns[i+1:]) for i in range(len(ns))):
             safe += 1
-            continue
-        for i in range(len(ns)):
-            t_ns = ns[:i] + ns[i + 1 :]
-            deltas = [a - b for a, b in zip(t_ns[:-1], t_ns[1:])]
-            if is_safe(deltas):
-                safe += 1
-                break
 
     return safe
