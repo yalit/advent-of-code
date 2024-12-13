058e275481b7891e7de22fcda0b45be5ecfabea4
Yannick <tech@yalit.be>
2024-12-13 12:15:08 +0100

    aoc : 2024 day 13 algebraic solution

--- aoc2024/day_13/aoc.py
+++ aoc2024/day_13/aoc.py
@@ -1,49 +1,40 @@
+import math
 import re
+
+
+def min_cost(lines: list[str], part2: bool) -> int:
+    ax, ay = list(
+        map(int, re.match(r"Button .: X\+(\d+), Y\+(\d+)", lines[0]).groups())
+    )
+    bx, by = list(
+        map(int, re.match(r"Button .: X\+(\d+), Y\+(\d+)", lines[1]).groups())
+    )
+    tx, ty = list(map(int, re.match(r"Prize: X=(\d+), Y=(\d+)", lines[2]).groups()))
+
+    if part2:
+        tx, ty = tx + 10000000000000, ty + 10000000000000
+
+    A = (tx * by - ty * bx) / (ax * by - ay * bx)
+    B = (tx - ax * A) / bx
+    if A > 0 and B > 0 and A == math.floor(A) and B == math.floor(B):
+        return int(A) * 3 + int(B)
+
+    return 0
 
 
 def handle_part_1(lines: list[str]) -> int:
     nb_tokens = 0
 
     for i in range(0, len(lines), 4):
-        ax, ay = list(
-            map(int, re.match(r"Button .: X\+(\d+), Y\+(\d+)", lines[i]).groups())
-        )
-        bx, by = list(
-            map(int, re.match(r"Button .: X\+(\d+), Y\+(\d+)", lines[i + 1]).groups())
-        )
-        tx, ty = list(
-            map(int, re.match(r"Prize: X=(\d+), Y=(\d+)", lines[i + 2]).groups())
-        )
-
-        memoirs = {}
-
-        def get_min_cost(na, nb):
-            x = ax * na + bx * nb
-            y = ay * na + by * nb
-            if x == tx and y == ty:
-                return na * 3 + nb
-
-            if x > tx or y > ty:
-                return float("inf")
-
-            if (x, y) in memoirs:
-                return memoirs[(x, y)]
-
-            min_cost = min(
-                get_min_cost(na + 1, nb),
-                get_min_cost(na, nb + 1),
-            )
-
-            memoirs[(x, y)] = min_cost
-
-            return min_cost
-
-        min_tokens = get_min_cost(0, 0)
-        if min_tokens < float("inf"):
-            nb_tokens += min_tokens
+        nb_tokens += min_cost(lines[i : i + 4], False)
 
     return nb_tokens
 
 
 def handle_part_2(lines: list[str]) -> int:
-    return 0
+    nb_tokens = 0
+
+    for i in range(0, len(lines), 4):
+        nb_tokens += min_cost(lines[i : i + 4], True)
+
+    return nb_tokens
