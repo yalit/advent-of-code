681a595d531afad325d815a07b0877a7595aaaa2
Yannick <tech@yalit.be>
2024-12-17 20:00:06 +0100

    refactor : 2024 day 17 (small refactor)

--- aoc2024/day_17/aoc.py
+++ aoc2024/day_17/aoc.py
@@ -1,3 +1,6 @@
+import time
+
+
 def get_registers_and_instructions(lines: list[str]) -> tuple[dict[str, int], list[int]]:
     return {
         'a': int(lines[0].split()[-1]),
@@ -5,51 +8,47 @@ def get_registers_and_instructions(lines: list[str]) -> tuple[dict[str, int], li
         'c': int(lines[2].split()[-1]),
     }, list(map(int, lines[4].split()[1].split(',')))
 
-def xor(a: int, b: int) -> int:
-    return a ^ b
-
 def run_instruction(registers: dict[str, int], operations: list[int]) -> list[int]:
     outputs = []
     pointer = 0
 
-    def get_operand_value(combo: bool) -> int:
-        operand = operations[pointer + 1]
-
-        if not combo or 0 <= operand <= 3:
-            return operand
+    def combo_value(value: int) -> int:
+        if 0 <= value <= 3:
+            return value
 
         op = {4: 'a', 5: 'b', 6: 'c'}
-        return registers[op[operand]]
+        return registers[op[value]]
 
     while pointer < len(operations):
         instruction = operations[pointer]
+        operand = operations[pointer + 1]
 
         steps = 2
         if instruction == 0:
-            registers['a'] = registers['a'] // (2 ** get_operand_value(True))
+            registers['a'] = registers['a'] // (2 ** combo_value(operand))
 
         elif instruction == 1:
-            registers['b'] = xor(registers['b'], get_operand_value(False))
+            registers['b'] = registers['b'] ^ operand
 
         elif instruction == 2:
-            registers['b'] = get_operand_value(True) % 8
+            registers['b'] = combo_value(operand) % 8
 
         elif instruction == 3:
             if registers['a'] != 0:
-                pointer = get_operand_value(False)
+                pointer = operand
                 continue
 
         elif instruction == 4:
-            registers['b'] = xor(registers['b'], registers['c'])
+            registers['b'] = registers['b'] ^ registers['c']
 
         elif instruction == 5:
-            outputs.append(get_operand_value(True) % 8)
+            outputs.append(combo_value(operand) % 8)
 
         elif instruction == 6:
-            registers['b'] = registers['a'] // (2 ** get_operand_value(True))
+            registers['b'] = registers['a'] // (2 ** combo_value(operand))
 
         elif instruction == 7:
-            registers['c'] = registers['a'] // (2 ** get_operand_value(True))
+            registers['c'] = registers['a'] // (2 ** combo_value(operand))
 
         pointer += steps
 
